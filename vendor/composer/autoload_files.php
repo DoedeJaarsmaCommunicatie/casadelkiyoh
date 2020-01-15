c6504fbcea779fd1edec161d6cf9cb477089d1e6
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-01-15 09:37:11 +0100

    Updated vendor

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -10,7 +10,7 @@ return array(
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
-    '89ff252b349d4d088742a09c25f5dd74' => $vendorDir . '/yahnis-elsts/plugin-update-checker/plugin-update-checker.php',
+    'eb560c198217526cfde8ceb63ae508de' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v4p8.php',
     'c39e5e966364f845b8dc03db0a1c135c' => $vendorDir . '/aristath/kirki/kirki.php',
     'c8f7179347ced746e5b8b3dbaf872eae' => $baseDir . '/includes/class-cdk-options.php',
     '0424fcff289c96ab0c7dc245204fd34d' => $baseDir . '/includes/class-cdk-model.php',
